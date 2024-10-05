ad3cec06a1ba37e37488de43f8dae13abb4332c1
KossiviEsse1 <kossivi14@gmail.com>
2024-10-05 15:12:30 -0500

    Completed Part 3 of exercise

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,16 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   const chores = ["Empty dishwasher", "Complete prep work", "Buy groceries"];
+
+   return (
+      <div className={classes.all}>
+         <h3 className={classes.choresHeading}>Chores List</h3>
+         <ul>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+            <li className={classes.choresText}>{chores[2]}</li>
+         </ul>
+      </div>
+   )
 }
