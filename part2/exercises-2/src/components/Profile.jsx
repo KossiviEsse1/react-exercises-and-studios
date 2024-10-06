dc6c6dfedf238386730e9d1ab2bdf6c8512f751e
KossiviEsse1 <kossivi14@gmail.com>
2024-10-06 12:04:49 -0500

    completed exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,25 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
 
 function Profile()
 {
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+   const listItem = oceans.map((ocean)=> {
+      return <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <h1>{ocean.name}</h1>
+         <img className='img' src={ocean.image} alt={ocean.name}></img>
+         <h3>Facts </h3>
+         <ul>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ul>
          <Button />
-      </>  
+      </div>
+   })
+   return(
+      {listItem}
    );
 }
 
